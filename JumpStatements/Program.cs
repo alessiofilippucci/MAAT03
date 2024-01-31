9e173ed60d7ad5c4fe6ceba6c3c66492a71d00f2
alessiofilippucci <info@alessiofilippucci.it>
2024-01-31 09:36:39 +0100

    Updated

---
+++ JumpStatements/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace JumpStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i == 10)
+            //    {
+            //        Console.WriteLine("Blocco for");
+            //        break;
+            //    }
+
+            //    Console.WriteLine("Dentro il for");
+            //}
+
+            //Console.WriteLine("Fuori dal for");
+
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine("Skippo for");
+            //        continue;
+            //    }
+
+            //    Console.WriteLine("Dentro il for");
+            //}
+
+            //Console.WriteLine("Fuori dal for");
+
+            //    string choice = Console.ReadLine();
+
+            //    if (choice == "1")
+            //        goto Test1;
+            //    else if (choice == "2")
+            //        goto Test2;
+            //    else if (choice == "3")
+            //        goto Test3;
+            //    else if (choice == "4")
+            //        goto Test4;
+
+            //    Test1:
+            //    Console.WriteLine("Test1");
+
+            //    Console.WriteLine("xxx");
+
+            //Test2:
+            //    Console.WriteLine("Test2");
+
+            //    Console.WriteLine("yyy");
+
+            //Test3:
+            //    Console.WriteLine("Test3");
+
+            //    Console.WriteLine("www");
+
+            //Test4:
+            //    Console.WriteLine("Test4");
+
+            //    Console.WriteLine("zzz");
+
+            //    switch (choice)
+            //    {
+            //        case "1":
+            //            break;
+            //        case "2": 
+            //            break;
+            //        case "3": 
+            //            break;
+            //        case "4": 
+            //            break;
+            //        case "5":
+            //            goto case "1";
+            //            break;
+            //        default:
+            //            break;
+            //    }
+
+            //Console.WriteLine("Vuoi uscire dall'applicazione?");
+
+            //bool quit = Console.ReadLine() == "si";
+
+            //if(quit)
+            //    return;
+
+            //Console.WriteLine("Continua...");
+
+            //Console.WriteLine("Inserisci un numero tra 0 e 50");
+
+            //try
+            //{
+            //    int num = int.Parse(Console.ReadLine());
+
+            //    if (num >= 0 && num <= 50)
+            //    {
+            //        Console.WriteLine("Ok, bravo!");
+            //    }
+            //    else
+            //    {
+            //        throw new Exception($"Il numero '{num}' non è compreso tra 0 e 50");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            //string name = Console.ReadLine();
+            //string lastname = Console.ReadLine();
+
+            //Console.WriteLine("Hello " + name + " " + lastname);
+            //Console.WriteLine("Hello {0} {1}", name, lastname);
+            //string welcomeMessage = string.Format("Hello {0} {1}", name, lastname);
+
+            //string welcomeMessage2 = $"Hello {name} {lastname}";
+
+        }
+    }
+}
