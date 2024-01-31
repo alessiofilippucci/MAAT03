9b133ec56089353cd6867dbe384f80f52941d4c1
alessiofilippucci <info@alessiofilippucci.it>
2024-01-31 10:31:35 +0100

    Updated

---
+++ Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** CALCOLO AREA CERCHIO ***");
+            Console.WriteLine();
+            Console.WriteLine("Inserisci il raggio: ");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = GetCircleArea(radius);
+
+            Console.WriteLine($"L'area è: {area}");
+        }
+
+        static double GetCircleArea(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+    }
+}
